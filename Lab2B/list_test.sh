3a9ed1211fe8d288ccf4d76780120913219e06ed
Mudith Mallajosyula <mudith@lnxsrv03.seas.ucla.edu>
2019-02-19 10:56:18 -0800

    updated Lab2B

--- Lab2B/list_test.sh
+++ Lab2B/list_test.sh
@@ -75,3 +75,18 @@ done
 
 
 
+#----------------------------------------------------------------------
+#----------------------------------------------------------------------
+
+#lab2b tests
+
+THREADS=(1 2 4 8 12 16 24)
+ITERATIONS=(1000)
+for k in ${THREADS[@]};
+do 
+	for l in ${ITERATIONS[@]};
+	do
+			./lab2_list --threads=$k --iterations=$l --sync=m>> lab2b_list.csv
+			./lab2_list --threads=$k --iterations=$l --sync=s>> lab2b_list.csv
+	done
+done
